537fd477751f5ab1497f38a9a0d6c3ac0de0cb9b
Andrew Fung <andrew.fungkinho@gmail.com>
2023-05-27 17:55:09 +0800

    Add project files.

---
+++ windowsApplication/Data/windowsAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace windowsApplication.Data
+{
+    public class windowsAppContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=master;Trusted_Connection=True;");
+        }
+    }
+}
